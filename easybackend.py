542e6ef02f92129e1a9106cf8ebf442d02941486
Twitch-Liang <73220500+Twitch-Liang@users.noreply.github.com>
2020-11-11 10:52:23 +0800

    20201111 do reply

--- easybackend.py
+++ easybackend.py
@@ -1,17 +1,39 @@
 from flask import Flask,request
+import requests
 
 app = Flask(__name__)    
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        data = request.get_json()
-        print(data)
-        
-        print('replyToken:',data['events'][0]['replyToken'])
-        
-        print('text:',data['events'][0]['message']['text'])
+        message = request.get_json()
+        print(message) 
+        replyToken = message['events'][0]['replyToken']      
+        print('replyToken:',replyToken)
+        text = message['events'][0]['message']['text']      
+        print('text:',text)
 
+        ####################################################
+        ####################reply user######################
+        ####################################################
+        accessToken='RUZ1+o+NDKgL7fMvnWovLobOXb0uODZztwcUFrirbtp2OvQqIqO+RMj2vb9ANr7j2Lp7e0Qhujw8RhCM5t1abqXb05K+DacExP8oPd6ONWosy1vbm5zq5gZRWriPHVvu24oBg37U+06b+NKX0sidjAdB04t89/1O/w1cDnyilFU='
+        headers={
+            'Content-Type':'application/json',
+            'Authorization':'Bearer '+accessToken
+        }
+        data ={
+            "replyToken":replyToken,
+            "messages":[
+            {
+                "type":"text",
+                "text":text
+            }
+        ]
+    }
+        url = 'https://api.line.me/v2/bot/message/reply'
+        r = requests.post(url,headers=headers,data=data)
+
+        ####################################################
 
         return "post"
     if request.method == 'GET':
