314d976ec45f86442090982c3be3eaf6da0b7839
Twitch-Liang <73220500+Twitch-Liang@users.noreply.github.com>
2020-11-12 14:48:59 +0800

    20201112 retry

--- Module/SchedulerWakeUp.py
+++ Module/SchedulerWakeUp.py
@@ -1,13 +1,19 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from datetime import datetime
-import time
+from apscheduler.triggers.interval import IntervalTrigger
+import requests
 
-def print_text():
-  print('Waken!')
+def sensor():
+  r = requests.get('https://sleepy-dawn-70030.herokuapp.com/')
+  print(r)
     
 
 def SchedulerWakeUp():
   sched = BackgroundScheduler(daemon=True)
-  sched.add_job(print_text,'interval',seconds=10,start_date='2020-11-12 00:00')
+  interval = IntervalTrigger(
+        seconds = 5,
+        start_date='2020-11-12 00:00:00',
+        end_date='2999-12-31 23:59:59',
+        timezone='Asia/Shanghai')
+  sched.add_job(sensor,trigger=interval)
   sched.start()
   return ''
--- easybackend.py
+++ easybackend.py
@@ -56,7 +56,7 @@ def index():
             ReplyMessage(replyToken,messages)
         return "post"
     if request.method == 'GET':
-        return "get"
+        return "I'm awake~"
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',port=5000,use_reloader=False)
+    app.run(host='127.0.0.1',port=5000)
