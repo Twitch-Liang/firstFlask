a476dfdd217883b0fcfc0c801053adcbb228f21a
Twitch-Liang <73220500+Twitch-Liang@users.noreply.github.com>
2020-11-12 09:28:42 +0800

    Update easybackend.py

--- easybackend.py
+++ easybackend.py
@@ -26,6 +26,11 @@ def index():
                         {
                             'type':'text',
                             'text':'電腦出'+fist[ai]+'，平手'
+                        },
+                        {
+                            "type": "sticker",
+                            "packageId": "11537",
+                            "stickerId": "52002773"
                         }
                     ]               
                 elif (ai == 0 and player == 1) or (ai == 1 and player == 2) or (ai == 2 and player ==0):
@@ -33,6 +38,11 @@ def index():
                         {
                             'type':'text',
                             'text':'電腦出'+fist[ai]+'，您贏了！'
+                        },
+                        {
+                            "type": "sticker",
+                            "packageId": "11537",
+                            "stickerId": "52002735"
                         } 
                     ] 
                 else:
@@ -40,6 +50,11 @@ def index():
                         {
                             'type':'text',
                             'text':'電腦出'+fist[ai]+'，電腦獲勝！'
+                        },
+                        {
+                            "type": "sticker",
+                            "packageId": "11537",
+                            "stickerId": "52002734"
                         }
                     ]
                 
