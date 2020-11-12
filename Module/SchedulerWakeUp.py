4fd250bd5f1c34404b865e889a1f74abbb14efd9
Twitch-Liang <73220500+Twitch-Liang@users.noreply.github.com>
2020-11-12 15:51:15 +0800

    Update SchedulerWakeUp.py

--- Module/SchedulerWakeUp.py
+++ Module/SchedulerWakeUp.py
@@ -8,6 +8,6 @@ def sensor():
 
 def SchedulerWakeUp():
   sched = BackgroundScheduler(daemon=True)
-  sched.add_job(sensor,'cron',day_of_week='0-6', hour='0-23', minute='0-59', second='0,10,20,30,40,50',start_date='2020-11-12')
+  sched.add_job(sensor,'cron',day_of_week='0-6', hour='0-23', minute='0-59', second='0,10,20,30,40,50',start_date='2020-11-12 00:00')
   sched.start()
   return ''
