6be65ff48a5220963ce1db83336f86cba6965ebd
Twitch-Liang <73220500+Twitch-Liang@users.noreply.github.com>
2020-11-13 09:39:08 +0800

    20201113 inclass

--- easybackend.py
+++ easybackend.py
@@ -36,90 +36,68 @@ def index():
                     'text':'googleNews'
                     }
                 ]
-            elif text == 'flex':
+            elif text == 'selfIntroduction':
                 messages =[
                     {
-  "type": "flex",
-  "altText": "this is a flex message",
-  "contents": {
-  "type": "carousel",
-  "contents": [
-    {
-      "type": "bubble",
-      "hero": {
-        "type": "image",
-        "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_5_carousel.png",
-        "size": "full",
-        "aspectRatio": "20:13",
-        "aspectMode": "cover"
-      },
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "spacing": "sm",
-        "contents": [
-          {
-            "type": "text",
-            "text": "Arm Chair, White",
-            "weight": "bold",
-            "size": "xl",
-            "wrap": True,
-            "contents": []
-          },
-          {
-            "type": "box",
-            "layout": "baseline",
-            "contents": [
-              {
-                "type": "text",
-                "text": "$49",
-                "weight": "bold",
-                "size": "xl",
-                "flex": 0,
-                "wrap": True,
-                "contents": []
-              },
-              {
-                "type": "text",
-                "text": ".99",
-                "weight": "bold",
-                "size": "sm",
-                "flex": 0,
-                "wrap": True,
-                "contents": []
-              }
-            ]
-          }
-        ]
-      },
-      "footer": {
-        "type": "box",
-        "layout": "vertical",
-        "spacing": "sm",
-        "contents": [
-          {
-            "type": "button",
-            "action": {
-              "type": "uri",
-              "label": "Add to Cart",
-              "uri": "https://linecorp.com"
-            },
-            "style": "primary"
-          },
-          {
-            "type": "button",
-            "action": {
-              "type": "uri",
-              "label": "Add to wishlist",
-              "uri": "https://linecorp.com"
-            }
-          }
-        ]
-      }
-    }
-  ]
-}
-}
+                        'type':'flex',
+                        'altText':'flex text of introduce myself',
+                        'contents':{
+                            'type':'bubble',
+                            'direction':'ltr',
+                            'header':{
+                                'type':'box',
+                                'layout':'vertical',
+                                'contents':[
+                                    {
+                                        'type':'text',
+                                        'text':'hello world~',
+                                        'size':'xl',
+                                        'color':'#15D573FF',
+                                        'align':'center',
+                                        'contents':[]
+                                    }
+                                ]
+                            },
+                            'hero':{
+                                'type':'image',
+                                'url':'https://www.vjgamer.com.hk/wp-content/uploads/2019/03/22/52331/40452668763_42996d9575_o-900x900.jpg',
+                                'size':'cover',
+                                'aspectRatio':'1.5:1',
+                                'aspectMode':'fit'
+                            },
+                            'body':{
+                                'type':'box',
+                                'layout':'vertical',
+                                'contents':[
+                                    {
+                                        'type':'text',
+                                        'text':'My name is Little_Twitch~',
+                                        'align':'center',
+                                        'contents':[
+                                            {
+                                                "type": "span",
+                                                "text": "I was born on 2020/11/10!"
+                                            },
+                                        ]
+                                    }
+                                ]
+                            },
+                            'footer':{
+                                'type':'box',
+                                'layout':'horizontal',
+                                'contents':[
+                                    {
+                                        'type':'button',
+                                        'action':{
+                                            'type':'uri',
+                                            'label':'button',
+                                            'uri':''
+                                        }
+                                    }
+                                ]
+                            }
+                        }
+                    }
                 ]
             else:
                 messages =[
