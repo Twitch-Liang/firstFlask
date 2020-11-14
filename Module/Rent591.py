12d38e7827bd4a0555c4c9e20992ed4176fe11e3
Twitch-Liang <73220500+Twitch-Liang@users.noreply.github.com>
2020-11-14 18:42:58 +0800

    Update Rent591.py

--- Module/Rent591.py
+++ Module/Rent591.py
@@ -13,81 +13,78 @@ def Rent591(text):
     cityName=random.choice(list(cityId.keys()))
   
   
-  if cityId.get(cityName) != None:
-    cookies={
-      'urlJumpIp':cityId.get(cityName)
-    }
-    url='https://rent.591.com.tw/?kind=0&region='+cityId.get(cityName)
-    r= requests.get(url,cookies=cookies)
+  
+  cookies={
+    'urlJumpIp':cityId.get(cityName)
+  }
+  url='https://rent.591.com.tw/?kind=0&region='+cityId.get(cityName)
+  r= requests.get(url,cookies=cookies)
 
-    soup=BeautifulSoup(r.text,'html.parser')
+  soup=BeautifulSoup(r.text,'html.parser')
 
-    houseCards=soup.select('.listInfo')
-    carousel={
-      'type': 'carousel',
-      'contents': [
-        
-      ]
-    }
-
-    count=1
-    for houseInfo in houseCards:
-      imageUrl = houseInfo.select('.imageBox img')[0].get('data-original')
-      titleArea = houseInfo.select('.infoContent h3 a')[0]
-      title = titleArea.text
-      infoUrl = 'https:'+titleArea.get('href')
-      bubble={
-          'type': 'bubble',
-          'direction': 'ltr',
-          'header': {
-            'type': 'box',
-            'layout': 'vertical',
-            'contents': [
-              {
-                'type': 'text',
-                'text': title ,
-                'align': 'center',
-                'contents': []
+  houseCards=soup.select('.listInfo')
+  carousel={
+    'type': 'carousel',
+    'contents': [
+      
+    ]
+  }
+  count=1
+  for houseInfo in houseCards:
+    imageUrl = houseInfo.select('.imageBox img')[0].get('data-original')
+    titleArea = houseInfo.select('.infoContent h3 a')[0]
+    title = titleArea.text
+    infoUrl = 'https:'+titleArea.get('href')
+    bubble={
+        'type': 'bubble',
+        'direction': 'ltr',
+        'header': {
+          'type': 'box',
+          'layout': 'vertical',
+          'contents': [
+            {
+              'type': 'text',
+              'text': title ,
+              'align': 'center',
+              'contents': []
+            }
+          ]
+        },
+        'hero': {
+          'type': 'image',
+          'url': imageUrl ,
+          'size': 'full',
+          'aspectRatio': '1.51:1'
+        },
+        'footer': {
+          'type': 'box',
+          'layout': 'horizontal',
+          'contents': [
+            {
+              'type': 'button',
+              'action': {
+                'type': 'uri',
+                'label': '查看詳情',
+                'uri': infoUrl
               }
-            ]
-          },
-          'hero': {
-            'type': 'image',
-            'url': imageUrl ,
-            'size': 'full',
-            'aspectRatio': '1.51:1'
-          },
-          'footer': {
-            'type': 'box',
-            'layout': 'horizontal',
-            'contents': [
-              {
-                'type': 'button',
-                'action': {
-                  'type': 'uri',
-                  'label': '查看詳情',
-                  'uri': infoUrl
-                }
-              }
-            ]
-          }
+            }
+          ]
         }
-      if count < 13:
-        carousel['contents'].append(bubble)
-      else:
-        break
-      count+=1
+      }
+    if count < 13:
+      carousel['contents'].append(bubble)
+    else:
+      break
+    count+=1
 
-    flexBox=[{
-      'type': 'flex',
-      'altText': 'Flex message of 591rent',
-      'contents': carousel
-    }]
-    # jsonData=json.dumps(carousel,ensure_ascii=False)
-    # f=open('rent.json','w',encoding='utf-8')
-    # f.write(jsonData)
-    # f.close()
+  flexBox=[{
+    'type': 'flex',
+    'altText': 'Flex message of 591rent',
+    'contents': carousel
+  }]
+  # jsonData=json.dumps(carousel,ensure_ascii=False)
+  # f=open('rent.json','w',encoding='utf-8')
+  # f.write(jsonData)
+  # f.close()
 
-    return flexBox
-  else:
-    return ''
+  return flexBox
