1e543fb47aff0898437ba0a1837d3c6dc2a9f6e5
Twitch-Liang <73220500+Twitch-Liang@users.noreply.github.com>
2020-11-19 11:45:53 +0800

    Update easybackend.py

--- easybackend.py
+++ easybackend.py
@@ -73,15 +73,23 @@ def index():
             elif text.find('球員/') != -1:
                 name = text.split('/')[1]
                 # print(time.now)
-                query = PlayersModel.query.filter_by(name=name).first()
+                # query = PlayersModel.query.filter_by(name=name).first()
                 # print(time.now)
-                # query = PlayersModel.query.filter(PlayersModel.name.ilike("%" + name + "%")).all()
-                messages =[
-                    {
-                    'type':'text',
-                    'text': f'{query.name}:{query.from_}~{query.to_}'
-                    }
-                ]
+                query = PlayersModel.query.filter(PlayersModel.name.ilike("%" + name + "%")).all()
+                if query != None:
+                    messages =[
+                        {
+                        'type':'text',
+                        'text': f'{query.name}:{query.from_}~{query.to_}'
+                        }
+                    ]
+                else:
+                     messages =[
+                        {
+                        'type':'text',
+                        'text': f'對不起！我沒找到與{name}相關的資料'
+                        }
+                    ]
 
             elif text == 'news' :
                 messages =[
