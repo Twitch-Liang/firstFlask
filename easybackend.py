58b5a6ff0c6b8182363e285bfb90e9b183c11a8c
Twitch-Liang <73220500+Twitch-Liang@users.noreply.github.com>
2020-11-19 11:32:13 +0800

    Update easybackend.py

--- easybackend.py
+++ easybackend.py
@@ -79,7 +79,7 @@ def index():
                 messages =[
                     {
                     'type':'text',
-                    'text':str(query)
+                    'text':str(query.all)
                     }
                 ]
 
