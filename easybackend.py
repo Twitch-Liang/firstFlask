11b9d06a445af71b65ad984249117c87c1363c8e
Twitch-Liang <73220500+Twitch-Liang@users.noreply.github.com>
2020-11-19 10:06:50 +0800

    20201119 test database

--- easybackend.py
+++ easybackend.py
@@ -2,11 +2,15 @@ from flask import request
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from bs4 import BeautifulSoup
+from datetime import time
+
+
 from Module.ReplyMessage import ReplyMessage
 from Module.FistGame import FistGame
 from Module.SchedulerWakeUp import SchedulerWakeUp
 from Module.Rent591 import  Rent591
 import requests
+
 
 
 
@@ -18,6 +22,32 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://rxtfpqtoiybpuv:6ab1c87f71ee64a09d7dc3700d8fcd0fb12eb0453302a75f8a22df1ffb971012@ec2-52-44-235-121.compute-1.amazonaws.com:5432/d5omf0mtm23h0b"
+
+class PlayersModel(db.Model):
+    __tablename__ = 'players'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=True)
+    from_ = db.Column(db.Integer, nullable=True)
+    to_ = db.Column(db.String, nullable=True)
+    position = db.Column(db.String, nullable=True)
+    height = db.Column(db.Float, nullable=True)
+    weight = db.Column(db.Float, nullable=True)
+    birthday = db.Column(db.DateTime, nullable=True)
+    college = db.Column(db.String, nullable=True)
+
+
+    def __init__(self, name, from_, to_, position, height, weight, birthday, college):
+        self.name = name
+        self.from_ = from_
+        self.to_ = to_
+        self.position = position
+        self.height = height
+        self.weight = weight
+        self.birthday = birthday
+        self.college = college
+
+
+
 
 db.init_app(app)
 
@@ -39,7 +69,13 @@ def index():
             fist =['剪刀','石頭','布']
             if text in fist:
                 messages=FistGame(text)
-                
+            
+            elif text.find('/球員') != -1:
+                name = text.split('/')[1]
+                print(datetime.strftime(time.now))
+                query = PlayersModel.query.filter_by(name=name).first()
+                print(datetime.strftime(time.now))
+
             elif text == 'news' :
                 messages =[
                     {
