85a7edfdb01f535e1bc5b828b871b399332ec7fd
Twitch-Liang <73220500+Twitch-Liang@users.noreply.github.com>
2020-11-19 14:00:45 +0800

    20201119 test

--- Module/ReplyMessage.py
+++ Module/ReplyMessage.py
@@ -4,7 +4,7 @@ import os
 
 
 def ReplyMessage(replyToken,messages):
-  accessToken='RUZ1+o+NDKgL7fMvnWovLobOXb0uODZztwcUFrirbtp2OvQqIqO+RMj2vb9ANr7j2Lp7e0Qhujw8RhCM5t1abqXb05K+DacExP8oPd6ONWosy1vbm5zq5gZRWriPHVvu24oBg37U+06b+NKX0sidjAdB04t89/1O/w1cDnyilFU='
+  accessToken=os.environ.get('ACCESS_TOKEN', None)
   headers={
       'Content-Type':'application/json',
       'Authorization':'Bearer '+accessToken
