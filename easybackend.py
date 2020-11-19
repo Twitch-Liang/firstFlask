b80e9a64827712ef0981290a6892b7d33801ef68
Twitch-Liang <73220500+Twitch-Liang@users.noreply.github.com>
2020-11-19 15:51:57 +0800

    give up

--- easybackend.py
+++ easybackend.py
@@ -63,7 +63,25 @@ def index():
                     
                 ]
             elif text.find('rent591') != -1 :
-                messages =Rent591(text)
+                name = text.split('/')[1]
+                # print(time.now)
+                # query = PlayersModel.query.filter_by(name=name).first()
+                # print(time.now)
+                query = PlayersModel.query.filter(PlayersModel.name.ilike("%" + name + "%")).first()
+                if query != None:
+                    messages =[
+                        {
+                        'type':'text',
+                        'text': f'{query.name}:{query.from_}~{query.to_}'
+                        }
+                    ]
+                else:
+                    messages =[
+                    {
+                    'type':'text',
+                    'text': f'對不起！我沒找到與{name}相關的資料QxQ'
+                    }
+                ]
 
             else:
                 messages =[
