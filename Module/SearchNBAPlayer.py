a5263abf957d66001a76a3071352ab1544eddafd
Twitch-Liang <73220500+Twitch-Liang@users.noreply.github.com>
2020-11-19 14:30:18 +0800

    20201119 test packing

---
+++ Module/SearchNBAPlayer.py
@@ -0,0 +1,21 @@
+def SearchNBAPlayer(text):
+  name = text.split('/')[1]
+  # print(time.now)
+  # query = PlayersModel.query.filter_by(name=name).first()
+  # print(time.now)
+  query = PlayersModel.query.filter(PlayersModel.name.ilike("%" + name + "%")).first()
+  if query != None:
+      messages =[
+          {
+          'type':'text',
+          'text': f'{query.name}:{query.from_}~{query.to_}'
+          }
+      ]
+  else:
+        messages =[
+          {
+          'type':'text',
+          'text': f'對不起！我沒找到與{name}相關的資料QxQ'
+          }
+      ]
+  return messages
--- easybackend.py
+++ easybackend.py
@@ -9,6 +9,7 @@ import os
 
 from Models.PlayersModel import PlayersModel
 
+from Module.SearchNBAPlayer import SearchNBAPlayer
 from Module.ReplyMessage import ReplyMessage
 from Module.FistGame import FistGame
 from Module.SchedulerWakeUp import SchedulerWakeUp
@@ -47,25 +48,7 @@ def index():
                 messages=FistGame(text)
             
             elif text.find('球員/') != -1:
-                name = text.split('/')[1]
-                # print(time.now)
-                # query = PlayersModel.query.filter_by(name=name).first()
-                # print(time.now)
-                query = PlayersModel.query.filter(PlayersModel.name.ilike("%" + name + "%")).first()
-                if query != None:
-                    messages =[
-                        {
-                        'type':'text',
-                        'text': f'{query.name}:{query.from_}~{query.to_}'
-                        }
-                    ]
-                else:
-                     messages =[
-                        {
-                        'type':'text',
-                        'text': f'對不起！我沒找到與{name}相關的資料QxQ'
-                        }
-                    ]
+                SearchNBAPlayer(text)
 
             elif text == 'news' :
                 messages =[
