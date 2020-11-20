ec71cba9c6c22f02d2aa59560e6cec7f2e5e6def
Twitch-Liang <73220500+Twitch-Liang@users.noreply.github.com>
2020-11-20 08:08:02 +0800

    Update SchedulerPushMessage.py

--- Module/SchedulerPushMessage.py
+++ Module/SchedulerPushMessage.py
@@ -25,7 +25,7 @@ def SchedulerPushMessage():
 
 def SchedulerSendPost():
   Scheduler = BackgroundScheduler(daemon=True)
-  Scheduler.add_job(SchedulerPushMessage,'cron',day_of_week='0-6', hour='8', minute='5', second='0')
+  Scheduler.add_job(SchedulerPushMessage,'cron',day_of_week='0-6', hour='8', minute='10', second='0')
   Scheduler.start()
   atexit.register(lambda: Scheduler.shutdown())
   return ''
