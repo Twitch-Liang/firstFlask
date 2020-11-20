fc0e465d54ede85f61fc0b891b5f5d834585bc68
Twitch-Liang <73220500+Twitch-Liang@users.noreply.github.com>
2020-11-20 09:36:19 +0800

    Update SchedulerWakeUp.py

--- Module/SchedulerWakeUp.py
+++ Module/SchedulerWakeUp.py
@@ -1,6 +1,5 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 import requests
-import atexit
 import json
 import os
 
@@ -35,5 +34,4 @@ def SchedulerWakeUp():
   sched.add_job(sensor,'cron',day_of_week='0-6', hour='0-23', minute='0,10,20,30,40,50', second='0',start_date='2020-11-12')
   sched.add_job(SchedulerPushMessage,'cron',day_of_week='0-6', hour='9', minute='40', second='0',start_date='2020-11-12',timezone='Asia/shanghai')
   sched.start()
-  atexit.register(lambda: sched.shutdown())
   return ''
